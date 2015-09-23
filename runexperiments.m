99ff8ef96be387f83572dda2aed58367a50c22df
Cassio M. M. Pereira <cassiomartini@gmail.com>
2015-09-23 16:06:16 -0300

    adding rng default

--- runexperiments.m
+++ runexperiments.m
@@ -1,6 +1,6 @@
 load_javaplex
-%load 'C:\Users\Cássio\Dropbox\workspace\subspacestream\dados\D8\subspace_stream.mat'
-load 'C:\Users\Cássio\Dropbox\workspace\subspacestream\dados\D16\subspace_stream.mat'
+load 'C:\Users\Cássio\workspace\subspacestream\dados\D8\subspace_stream.mat'
+% load 'C:\Users\Cássio\Dropbox\workspace\subspacestream\dados\D16\subspace_stream.mat'
 %load 'C:\Users\Cássio\Dropbox\workspace\subspacestream\dados\D32\subspace_stream.mat'
 %  load 'C:\Users\Cássio\Dropbox\workspace\subspacestream\dados\D64\subspace_stream.mat'
 % load 'C:\Users\Cássio\Dropbox\workspace\subspacestream\dados\D128\subspace_stream.mat'
@@ -8,6 +8,8 @@ load 'C:\Users\Cássio\Dropbox\workspace\subspacestream\dados\D16\subspace_stream
 MAXITERS = 2;
 RATIO = 10;
 DIVISIONS = 1000;
+
+rng default;
 
 Q = ptsstream(X, sup, 1000, 2, 50, MAXITERS, RATIO, DIVISIONS);
 % save Q.mat Q
